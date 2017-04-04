f9b9c5286df896e9fa707e86201d6041a270ab36
BEcraft <becraftmcpe@gmail.com>
2017-04-03 21:43:49 -0400

    Game

---
+++ src/BEcraft/Minigame/task/SignTask.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BEcraft\Minigame\task;
+
+use pocketmine\Server;
+use pocketmine\scheduler\PluginTask;
+use pocketmine\tile\Sign;
+use pocketmine\utils\Config;
+use BEcraft\Minigame\Main;
+use pocketmine\utils\TextFormat as T;
+
+class SignTask extends PluginTask{
+	
+	public function __construct(Main $main){
+	parent::__construct($main);
+	$this->plugin = $main;
+	}
+	
+	public function onRun($tick){
+	$world = $this->plugin->getServer()->getDefaultLevel();
+	$tiles = $world->getTiles();
+	foreach($tiles as $sign){
+		if($sign instanceof Sign){
+			$text = $sign->getText();
+			$prefix = T::GRAY."[".T::YELLOW."Practice".T::GRAY."]".T::RESET;
+			if($text[0] == "practice"){
+				$game = $text[1];
+				if($this->plugin->arenaExists($game)){
+					$config = new Config($this->plugin->getDataFolder()."Arenas/".$game.".yml", Config::YAML);
+					$name = $config->get("Name");
+					$level = $config->get("Level");
+					$cantida = count(Server::getInstance()->getLevelByName($level)->getPlayers());
+					if(count($this->plugin->getServer()->getLevelByName($level)->getPlayers()) == 1){
+						$sign->setText($prefix, $name, T::AQUA.$level, T::YELLOW.$cantida."/2");
+						}else if(count($this->plugin->getServer()->getLevelByName($level)->getPlayers()) >= 2){
+							$sign->setText($prefix, $name, T::AQUA.$level, T::RED."Running");
+							}
+					}
+				}
+			}
+		}
+	
+	}
+	}
